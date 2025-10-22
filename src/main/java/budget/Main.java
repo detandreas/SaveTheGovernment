29618faf4c5a691ecba34b43194edae1051b81b9
Andreas Papathanasiou <t8240105@aueb.gr>
2025-10-22 22:53:22 +0300

    Add checkstyle plugin
    Updated pom.xml to include checkstyle
    created Checkstyle/suppresions.xml to suppress some checkstyle features --> JavadocPackage
    Modified Main.java to comply with checkstyle rules

--- src/main/java/budget/Main.java
+++ src/main/java/budget/Main.java
@@ -2,8 +2,17 @@ package budget;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+/**
+ * Entry point της εφαρμογής CLI.
+ */
+public final class Main {
+
+    private Main() { }
+    /**
+     * Main method της εφαρμογής CLI.
+     * @param args ορίσματα γραμμής εντολών
+     */
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your username: ");
         String name = input.nextLine();
