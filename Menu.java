204ed8c597dfe0dcd6f03e1ac36a8e58b17f3300
patatoukakinancy <t8240258@aueb.gr>
2025-10-25 18:52:30 +0300

    delete constants.java

---
+++ Menu.java
@@ -0,0 +1,68 @@
+public class Menu {
+
+    //Role Selection Menu
+    public static final String[] ROLE_SELECTION_MENU = {
+        "Please select your Role:",
+        "1. Citizen",
+        "2. Government Member",
+        "3. Prime Minister"
+    };
+
+    //Citizen Main Menu
+    public static final String[] CITIZEN_MAIN_MENU = {
+        "Please select an option: ",
+        "1. View total budget",
+        "2. View change history",
+        "3. View statistics",
+        "4. Logout."
+    };
+
+    //Government Member Main Menu
+    public static final String[] GOVERNMENT_MEMBER_MAIN_MENU = {
+        "Please select an option: ",
+        "1. View total budget",
+        "2. View change history",
+        "3. View statistics",
+        "4. Submit change request for a budget item",
+        "5. View your request history",
+        "6. Logout"
+    };
+
+    //Ministry of Finance Member Main Menu
+    public static final String[] FINANCE_MEMBER_MAIN_MENU = {
+        "Please select an option: ",
+        "1. View total budget",
+        "2. View change history",
+        "3. View statistics",
+        "4. Submit change request for a budget item",
+        "5. Manage pending change requests",
+        "6. View your request history",
+        "7. Logout"
+    };
+
+    //Prime Minister Main Menu
+    public static final String[] PRIME_MINISTER_MAIN_MENU = {
+        "Please select an option: ",
+        "1. View total budget",
+        "2. View change history",
+        "3. View statistics",
+        "4. Manage pending change requests",
+        "5. Logout"
+    };
+
+    //Pending Requests Submenu - Prime Minister
+    public static final String[] PENDING_REQUESTS_SUBMENU = {
+        "Please select an option: ",
+        "1. Approve request",
+        "2. Reject request",
+        "3. Return to main menu"
+    };
+
+    //Confirmation Submenu for Change Requests
+    public static final String[] CONFIRMATION_SUBMENU = {
+        "Are you sure you want to submit the request for Prime Minister approval?",
+        "1. YES",
+        "2. NO"
+    };
+ 
+}
