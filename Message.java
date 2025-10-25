00672995f06caf7beb428c197209c8ad69b3724e
patatoukakinancy <t8240258@aueb.gr>
2025-10-25 17:02:48 +0300

    feat: add welcome-login messages

---
+++ Message.java
@@ -0,0 +1,32 @@
+public class Message {
+
+    //Welcome message
+    public static final String WELCOME_MESSAGE = "Welcome to the Government Budget application!";
+    
+    //Login messages
+    public static final String[] ROLE_SELECTION_MENU = {
+        "Please, select your Role:",
+        "1. Citizen",
+        "2. Government Member",
+        "3. Prime Minister"
+    };
+
+    public static final String INVALID_OPTION = "Invalid option. Please try again.";
+
+    public static final String LOGIN_MESSAGE = "Please, enter your username and your password.";
+
+    public static final String USERNAME_PROMPT = "Username: ";
+
+    public static final String PASSWORD_PROMPT = "Password: ";
+
+    public static final String LOGIN_SUCCESS = "Successful login! Welcome, %s ";
+
+    public static final String LOGIN_FAILED = "Login failed. Invalid username or password.";
+
+    
+
+
+
+
+
+}
