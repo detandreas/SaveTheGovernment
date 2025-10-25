0877a0d4ad8897090d160202b867facf5923ae4a
patatoukakinancy <t8240258@aueb.gr>
2025-10-25 18:49:54 +0300

    delete constants.java

---
+++ src/main/java/budget/util/Constants/Message.java
@@ -0,0 +1,24 @@
+public class Message {
+
+    //Welcome Message
+    public static final String WELCOME_MESSAGE = "Welcome to the Government Budget application!";
+    
+    //Login Messages
+    public static final String INVALID_OPTION = "Invalid option. Please try again.";
+
+    public static final String LOGIN_MESSAGE = "Please, enter your username and your password.";
+
+    public static final String USERNAME_PROMPT = "Username: ";
+
+    public static final String PASSWORD_PROMPT = "Password: ";
+
+    public static final String LOGIN_SUCCESS = "Successful login! Welcome, %s ";
+
+    public static final String LOGIN_FAILED = "Login failed. Invalid username or password.";
+
+    //Change Request Messages - Government Member
+    public static final String SELECT_ITEM_MESSAGE = "Choose the Budget Item to modify.";
+
+    public static final String NEW_AMOUNT = "Enter new amount for %s :";
+
+}
