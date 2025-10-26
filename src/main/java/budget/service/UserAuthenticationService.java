338a8129ff6bc7e3f7793750b7268f1dfb11b6cb
aggelosnais <t8240091@aueb.gr>
2025-10-26 22:50:28 +0200

    correcting the tests

--- src/main/java/budget/service/UserAuthenticationService.java
+++ src/main/java/budget/service/UserAuthenticationService.java
@@ -24,63 +24,6 @@ public class UserAuthenticationService {
         this.userRepository = userRepository;
         this.currentUser = null;
     }
-    /**
-     * User tries to connect with username/password.
-     * @return true if connection is successful, false otherwise
-     */
-    public boolean login(String username, String password) {
-        Optional<User> userOpt = userRepository.findByUsername(username);
-
-        if (userOpt.isPresent()) {
-            User user = userOpt.get();
-            if (user.getPassword().equals(hashPassword(password))) {
-                this.currentUser = user;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Disconnects current user.
-     */
-    public void logout() {
-        this.currentUser = null;
-    }
-
-    /**
-     * returns the current connected user.
-     */
-    public User getCurrentUser() {
-        return this.currentUser;
-    }
-
-    /**
-     * checks if there is connected user .
-     */
-    public boolean isAuthenticated() {
-        return this.currentUser != null;
-    }
-     /**
-     * Encryption of password with SHA-256.
-     */
-    public String hashPassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(password.getBytes());
-            StringBuilder hexString = new StringBuilder();
-
-            for (byte b : hash) {
-                hexString.append(String.format("%02x", b));
-            }
-
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Error during hashing of password.", e);
-        }
-    }
-}
-
 
     /**
      * Attempts to authenticate a user with the provided credentials.
@@ -94,7 +37,7 @@ public class UserAuthenticationService {
 
         if (userOpt.isPresent()) {
             User user = userOpt.get();
-            if (user.getHashPassword().equals(hashPassword(password))) {
+            if (user.getPassword().equals(hashPassword(password))) {
                 this.currentUser = user;
                 return true;
             }
