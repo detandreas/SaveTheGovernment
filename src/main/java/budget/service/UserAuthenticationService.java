7b1bd8d1a6737f9eeaeccfa318688b96bebb356f
aggelosnais <t8240091@aueb.gr>
2025-10-26 21:37:59 +0200

    Implement login, logout, currentUser retrieval and authentication methods

--- src/main/java/budget/service/UserAuthenticationService.java
+++ src/main/java/budget/service/UserAuthenticationService.java
@@ -24,6 +24,44 @@ public class UserAuthenticationService {
         this.userRepository = userRepository;
         this.currentUser = null;
     }
+    /**
+     * User tries to connect with username/password.
+     * @return true if connection is successful, false otherwise
+     */
+    public boolean login(String username, String password) {
+        Optional<User> userOpt = userRepository.findByUsername(username);
+
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            if (user.getPassword().equals(hashPassword(password))) {
+                this.currentUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Disconnects current user.
+     */
+    public void logout() {
+        this.currentUser = null;
+    }
+
+    /**
+     * returns the current connected user.
+     */
+    public User getCurrentUser() {
+        return this.currentUser;
+    }
+
+    /**
+     * checks if there is connected user .
+     */
+    public boolean isAuthenticated() {
+        return this.currentUser != null;
+    }
+
 
     /**
      * Attempts to authenticate a user with the provided credentials.
