64e8e88b13259a0767314956709793bbbfa820f7
aggelosnais <t8240091@aueb.gr>
2025-10-26 21:10:54 +0200

    Add UserRepository dependency and currentUser tracking

--- src/main/java/budget/service/UserAuthenticationService.java
+++ src/main/java/budget/service/UserAuthenticationService.java
@@ -1,4 +1,18 @@
 package budget.service;
 
+import budget.model.user.User;
+import budget.repository.UserRepository;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
 public class UserAuthenticationService {
-}
+
+    private final UserRepository userRepository;
+    private User currentUser;
+
+    public UserAuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.currentUser = null;
+    }
+
