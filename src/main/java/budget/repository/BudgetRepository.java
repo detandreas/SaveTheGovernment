32f84f7a5c93330178de65dcc0186eeb6e973e68
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-05 11:43:35 +0200

    Clear contents of BudgetRepository.java

--- src/main/java/budget/repository/BudgetRepository.java
+++ src/main/java/budget/repository/BudgetRepository.java
@@ -1,29 +1,2 @@
 package budget.repository;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
-import budget.model.domain.Budget;
-
-/**
- * Repository class for managing budget data. Handles loading, saving, and
- * validation of files from JSON storage.
- * */
-
-public class BudgetRepository implements GenericInterfaceRepository<Budget, Integer> {
-    
-
-}
