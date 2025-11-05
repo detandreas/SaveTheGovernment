6342ac23b918637a7e7f8052f2b5c4182f38ed61
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-05 11:07:34 +0200

    fix: adding messages and correcting whitspace erros

--- src/main/java/budget/repository/GenericInterfaceRepository.java
+++ src/main/java/budget/repository/GenericInterfaceRepository.java
@@ -1,8 +1,15 @@
 package budget.repository;
 import java.util.ArrayList;
 
-public interface GenericInterfaceRepository <T, ID> {
-    ArrayList<T> load(); //Φορτωση ολων των δεδομενων
+/**
+ * Generic interface for basic CRUD operations
+ */
+public interface GenericInterfaceRepository<T, ID> {
+    /** Loads all data */
+    ArrayList<T> load();
+    
+    /** Saves an entity */
     void save(T entity); 
+    /** Checks if an entity exists by ID */
     boolean exist(ID id);
 }
