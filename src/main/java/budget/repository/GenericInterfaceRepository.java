d90bdf8b0231d2b2e8e283ac3a60c1925d9bbd17
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-05 15:13:45 +0200

    fix checkstyle errors

--- src/main/java/budget/repository/GenericInterfaceRepository.java
+++ src/main/java/budget/repository/GenericInterfaceRepository.java
@@ -2,14 +2,24 @@ package budget.repository;
 import java.util.ArrayList;
 
 /**
- * Generic interface for basic CRUD operations
+ * Generic interface for basic CRUD operations 
+ * @param <T>  the type of entity handled by the repository
+ * @param <ID> the type of the unique identifier for the entity
  */
 public interface GenericInterfaceRepository<T, ID> {
-    /** Loads all data */
+    /** Loads all data 
+     * @return an {@link ArrayList} containing all entities
+    */
     ArrayList<T> load();
     
-    /** Saves an entity */
+    /** Saves an entity 
+     * @param entity the entity to be saved
+    */
     void save(T entity); 
-    /** Checks if an entity exists by ID */
+    
+    /** Checks if an entity exists by ID 
+     * @param id the identifier of the entity
+     * @return {@code true} if the entity exists; {@code false} otherwise
+    */
     boolean exist(ID id);
 }
