049c89ae097435634a87bbdabfe8d0663cb6c1e3
SteliosVe <t8230015@aueb.gr>
2025-11-05 05:00:04 +0200

    Η Test τησ Citizen

--- src/test/java/budget/model/domain/user/TestCitizen.java
+++ src/test/java/budget/model/domain/user/TestCitizen.java
@@ -1,6 +1,43 @@
 package budget.model.domain.user;
 
-import budget.model.domain.user.Citizen;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+import budget.model.domain.BudgetItem;
+import budget.model.enums.Ministry;
 
 public class TestCitizen {
+    String username="Stelios123";
+    String fullname="SteliosVeisakis";
+    String password="Stelios123";
+    List<Ministry> ministries = new ArrayList<>();
+
+    BudgetItem tip=new BudgetItem(823, 2005, username, 10000,true, ministries);
+    Citizen Stelios= new Citizen(username, fullname, password);
+
+    @Test
+    void TestConstructor(){
+        assertEquals("Stelios123",Stelios.getUserName());
+        assertEquals("SteliosVeisakis",Stelios.getFullName());
+        assertEquals("Stelios123",Stelios.getHashPassword());
+        assertNotNull(Stelios.getId());
+    }
+
+    @Test 
+    void TestCanApprove(){
+        assertFalse(Stelios.canApprove());
+    }
+
+    @Test 
+    void TestCanEdit(){
+        assertFalse(Stelios.canEdit(tip));
+    }
+
+
+    
 }
