f967ca1ce23d38f93e333e86bccab05c8bf596d2
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-06 02:56:07 +0200

    fix bugs

--- src/main/java/budget/repository/GenericInterfaceRepository.java
+++ src/main/java/budget/repository/GenericInterfaceRepository.java
@@ -2,24 +2,24 @@ package budget.repository;
 import java.util.ArrayList;
 
 /**
- * Generic interface for basic CRUD operations 
- * @param <T>  the type of entity handled by the repository
- * @param <ID> the type of the unique identifier for the entity
+ * Generic interface for basic CRUD operations.
+ * @param <T>  the type of entity handled by the repository.
+ * @param <ID> the type of the unique identifier for the entity.
  */
 public interface GenericInterfaceRepository<T, ID> {
-    /** Loads all data 
-     * @return an {@link ArrayList} containing all entities
+    /** Loads all data. 
+     * @return an {@link ArrayList} containing all entities.
     */
     ArrayList<T> load();
     
-    /** Saves an entity 
-     * @param entity the entity to be saved
+    /** Saves an entity. 
+     * @param entity the entity to be saved.
     */
     void save(T entity); 
     
-    /** Checks if an entity exists by ID 
-     * @param id the identifier of the entity
-     * @return {@code true} if the entity exists; {@code false} otherwise
+    /** Checks if an entity exists by ID. 
+     * @param id the identifier of the entity.
+     * @return {@code true} if the entity exists; {@code false} otherwise.
     */
     boolean exist(ID id);
 }
