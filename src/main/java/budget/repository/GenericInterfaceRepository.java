0b71e5a918d5d0e45a9b4d273cae54a3e70e65d5
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-06 03:07:02 +0200

    fix bugs

--- src/main/java/budget/repository/GenericInterfaceRepository.java
+++ src/main/java/budget/repository/GenericInterfaceRepository.java
@@ -7,17 +7,15 @@ import java.util.ArrayList;
  * @param <ID> the type of the unique identifier for the entity.
  */
 public interface GenericInterfaceRepository<T, ID> {
-    /** Loads all data. 
+    /** Loads all data.
      * @return an {@link ArrayList} containing all entities.
     */
     ArrayList<T> load();
-    
-    /** Saves an entity. 
+    /** Saves an entity.
      * @param entity the entity to be saved.
     */
-    void save(T entity); 
-    
-    /** Checks if an entity exists by ID. 
+    void save(T entity);
+    /** Checks if an entity exists by ID.
      * @param id the identifier of the entity.
      * @return {@code true} if the entity exists; {@code false} otherwise.
     */
