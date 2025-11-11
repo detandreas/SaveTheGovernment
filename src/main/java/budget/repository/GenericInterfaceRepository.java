d4f1949235a1b1d650cb3d63cbe285352780a83f
Andreas Papathanasiou <t8240105@aueb.gr>
2025-11-11 14:22:34 +0200

    Add new methods to GenericInterfaceRepository class

--- src/main/java/budget/repository/GenericInterfaceRepository.java
+++ src/main/java/budget/repository/GenericInterfaceRepository.java
@@ -1,5 +1,6 @@
 package budget.repository;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Generic interface for basic CRUD operations.
@@ -8,16 +9,31 @@ import java.util.List;
  */
 public interface GenericInterfaceRepository<T, ID> {
     /** Loads all data.
-     * @return an {@link ArrayList} containing all entities.
+     * @return an {@link List} containing all entities.
     */
     List<T> load();
+
     /** Saves an entity.
      * @param entity the entity to be saved.
     */
     void save(T entity);
+
     /** Checks if an entity exists by ID.
      * @param id the identifier of the entity.
      * @return {@code true} if the entity exists; {@code false} otherwise.
     */
-    boolean exist(ID id);
+    boolean existsById(ID id);
+
+    /**
+     * Deletes an entity from the repository.
+     * @param entity the entity to be deleted.
+     */
+    void delete(T entity);
+
+     /**
+     * Finds an entity by its identifier.
+     * @param id the unique identifier of the entity to search for.
+     * @return an Optional containing the entity if found, otherwise an empty Optional.
+     */
+    Optional<T> findById(ID id);
 }
