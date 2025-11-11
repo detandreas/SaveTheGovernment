1a18dad0f13a96e43178b0b76d0491771cfc1f3b
aggelosnais <t8240091@aueb.gr>
2025-11-11 23:25:57 +0200

    feat: Create ChangeLogRepository Class

--- src/main/java/budget/repository/ChangeLogRepository.java
+++ src/main/java/budget/repository/ChangeLogRepository.java
@@ -1,1 +1,24 @@
 package budget.repository;
+
+import budget.model.domain.ChangeLog;
+import budget.util.PathsUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Repository class for managing ChangeLog records.
+ * This class provides basic create,read,update,delete operations for ChangeLog objects,
+ * persisting them to a JSON file using Gson.
+ */
+public class ChangeLogRepository {
+
+}
