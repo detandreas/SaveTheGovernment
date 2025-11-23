4da660e4f40bba91c0a2d3faf41382b441c84578
aggelosnais <t8240091@aueb.gr>
2025-11-23 10:55:24 +0200

    feat:Create ChangeLogService

--- src/main/java/budget/service/ChangeLogService.java
+++ src/main/java/budget/service/ChangeLogService.java
@@ -1,4 +1,17 @@
 package budget.service;
 
+import budget.model.domain.BudgetItem;
+import budget.model.domain.ChangeLog;
+import budget.repository.ChangeLogRepository;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/**
+ * Service responsible for storing and retrieving {@link ChangeLog}
+ * entries. This service is stateless and delegates persistence to
+ * {@link ChangeLogRepository}.
+ */
 public class ChangeLogService {
 }
