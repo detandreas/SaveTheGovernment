26ae10bb9a2989193d1de185e6a38cf66995fc3b
Ioannis Kolovatsios <t8240268@aueb.gr>
2025-11-25 01:05:21 +0200

    feat add imports and BudgetService constructor

--- src/main/java/budget/service/BudgetService.java
+++ src/main/java/budget/service/BudgetService.java
@@ -1,4 +1,39 @@
 package budget.service;
 
+import budget.model.domain.Budget;
+import budget.model.domain.BudgetItem;
+import budget.model.domain.Ministry;
+import budget.model.user.User;
+import budget.repository.BudgetRepository;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Service class for managing budgets.
+ */
 public class BudgetService {
+
+    private static final Logger LOGGER =
+            Logger.getLogger(BudgetService.class.getName());
+    private static final Object LOCK = new Object();
+
+    private final BudgetRepository budgetRepository;
+    private final UserAuthorizationService userAuthorizationService;
+    private final ChangeLogService changeLogService;
+    private final ChangeRequestService changeRequestService;
+
+    public BudgetService(BudgetRepository budgetRepository,
+                        UserAuthorizationService userAuthorizationService,
+                        ChangeLogService changeLogService,
+                        ChangeRequestService changeRequestService) {
+        this.budgetRepository = budgetRepository;
+        this.userAuthorizationService = userAuthorizationService;
+        this.changeLogService = changeLogService;
+        this.changeRequestService = changeRequestService;
+                        }
 }
