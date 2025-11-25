1965bccaf5d9b11133d1f4917bb3c581c7aa306f
Charalampos Stamatoglou <t8240133@aueb.com>
2025-11-25 12:47:26 +0200

    Add few more imports

--- src/test/java/budget/util/TestInputValidator.java
+++ src/test/java/budget/util/TestInputValidator.java
@@ -3,6 +3,8 @@ package budget.util;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.UUID;
+
 public class TestInputValidator {    
     
     @Test
