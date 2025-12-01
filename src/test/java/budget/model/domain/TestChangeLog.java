96782ebf67de750ed0fe7476b7dcc6342f9845fd
Andreas Papathanasiou <t8240105@aueb.gr>
2025-12-02 00:06:08 +0200

    Fix wildcard import on TestChangeLog

--- src/test/java/budget/model/domain/TestChangeLog.java
+++ src/test/java/budget/model/domain/TestChangeLog.java
@@ -3,8 +3,9 @@ package budget.model.domain;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestChangeLog {
 
