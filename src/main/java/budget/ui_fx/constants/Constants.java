5adc59ce4a0b78d3e58fbcd3f9ca8ef17fde539d
Andreas Papathanasiou <t8240105@aueb.gr>
2025-12-13 12:42:56 +0200

    Add new Constants class for UI

---
+++ src/main/java/budget/ui_fx/constants/Constants.java
@@ -0,0 +1,20 @@
+package budget.ui_fx.constants;
+
+public final class Constants {
+    /**
+     * constructor.
+     */
+    private Constants() {
+        // prevents initialization
+    }
+    public static final String FINANCE_GOV_VIEW =
+                            "/view/FinanceGovMemberDashboardView.fxml";
+    public static final String GOV_VIEW =
+                            "/view/GovMemberDashboardView.fxml";
+    public static final String CITIZEN_VIEW =
+                            "/view/CitizenDashboardView.fxml";
+    public static final String PRIME_MINISTER_VIEW =
+                            "/view/PrimeMinisterDashboardView.fxml";
+    public static final String WELCOME_VIEW =
+                            "/view/WelcomeView.fxml";
+}
