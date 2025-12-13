421734dab6d1e1b73d8937278c9272385b53f815
Andreas Papathanasiou <t8240105@aueb.gr>
2025-12-13 12:44:48 +0200

    Optimize AccountCreationController, AccountCreationView
    -Smaller methods
    - DRY methods

--- src/main/java/budget/ui_fx/controller/AccountCreationController.java
+++ src/main/java/budget/ui_fx/controller/AccountCreationController.java
@@ -1,30 +1,34 @@
 package budget.ui_fx.controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import javafx.util.StringConverter;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import budget.ui_fx.util.SceneLoader;
 import budget.constants.Message;
 import budget.exceptions.ValidationException;
-import budget.model.enums.UserRole;
 import budget.model.domain.user.Citizen;
 import budget.model.domain.user.GovernmentMember;
 import budget.model.domain.user.PrimeMinister;
 import budget.model.domain.user.User;
 import budget.model.enums.Ministry;
+import budget.model.enums.UserRole;
+import budget.repository.UserRepository;
 import budget.service.InputValidationService;
 import budget.service.UserAuthenticationService;
-import budget.repository.UserRepository;
+import budget.ui_fx.util.SceneLoader;
+import budget.util.InputValidator;
+import budget.util.PasswordUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 
 public class AccountCreationController {
 
@@ -34,12 +38,12 @@ public class AccountCreationController {
     @FXML private Label successLabel;
 
     // FXML UI Text Fields
-    @FXML private TextField usernameField;
-    @FXML private PasswordField passwordField;
+    @FXML private TextField usernameTextField;
+    @FXML private TextField fullNameTextField;
+    @FXML private TextField visiblePasswordTextField;
+    @FXML private TextField confirmVisiblePasswordTextField;
     @FXML private PasswordField confirmPasswordField;
-    @FXML private TextField fullNameField;
-    @FXML private TextField visiblePasswordField;
-    @FXML private TextField confirmVisiblePasswordField;
+    @FXML private PasswordField passwordField;
 
     // FXML UI Buttons
     @FXML private Button createAccountButton;
@@ -57,12 +61,14 @@ public class AccountCreationController {
 
     // FXML UI Container
     @FXML private HBox ministryContainer;
-    
 
-    private final String EYE_OPEN_PATH = "/images/eye_open.png";
-    private final String EYE_CLOSED_PATH = "/images/eye_closed.png";
-    
-    private final UserAuthenticationService authService = 
+
+    private static final String EYE_OPEN_PATH = "/images/eye_open.png";
+    private static final String EYE_CLOSED_PATH = "/images/eye_closed.png";
+    private static final Logger LOGGER =
+                Logger.getLogger(AccountCreationController.class.getName());
+
+    private final UserAuthenticationService authService =
         new UserAuthenticationService(new UserRepository()
     );
 
@@ -71,47 +77,18 @@ public class AccountCreationController {
      */
     @FXML
     public void initialize() {
-        createAccountLabel.setText(Message.CREATE_ACCOUNT_MESSAGE);
-        usernameField.setPromptText(Message.SIGNUP_ENTER_USERNAME);
-        passwordField.setPromptText(Message.SIGNUP_ENTER_PASSWORD);
-        confirmPasswordField.setPromptText(Message.SIGNUP_CONFIRM_PASSWORD);
-        fullNameField.setPromptText(Message.SIGNUP_ENTER_FULLNAME);
-        roleComboBox.setPromptText(Message.SIGNUP_SELECT_ROLE);
-        ministryComboBox.setPromptText(Message.SIGNUP_SELECT_DEPARTMENT);
-        createAccountButton.setText(Message.CREATE_ACCOUNT_BUTTON);
-        cancelButton.setText(Message.CANCEL_BUTTON);
-        backButton.setText(Message.BACK_BUTTON);
-        passwordField.textProperty().bindBidirectional(visiblePasswordField.textProperty());
-        confirmPasswordField.textProperty().bindBidirectional(confirmVisiblePasswordField.textProperty());
+        passwordField.textProperty()
+                    .bindBidirectional(visiblePasswordTextField.textProperty());
+        confirmPasswordField.textProperty()
+                    .bindBidirectional(confirmVisiblePasswordTextField
+                                        .textProperty());
 
         roleComboBox.getItems().setAll(UserRole.values());
         ministryComboBox.getItems().setAll(Ministry.values());
 
-        roleComboBox.setConverter(new StringConverter<UserRole>() {
-            @Override
-            public String toString(UserRole role) {
-                if (role == null) return null;
-                return role.getDisplayName(); 
-            }
-
-            @Override
-            public UserRole fromString(String string) {
-                return null; 
-            }
-        });
-
-        ministryComboBox.setConverter(new StringConverter<Ministry>() {
-            @Override
-            public String toString(Ministry ministry) {
-                if (ministry == null) return null;
-                return ministry.getDisplayName(); 
-            }
-            @Override
-            public Ministry fromString(String string) { 
-                return null;
-            }
-        });
-
+        // προσθήκη listener γισ handling της περίπτωσης
+        // που ο χρήστης διαλέγει role govMember
+        // και επαναφορά
         roleComboBox.valueProperty().addListener((obs, oldVal, newVal) -> {
             if (newVal == UserRole.GOVERNMENT_MEMBER) {
                 ministryContainer.setVisible(true);
@@ -122,142 +99,222 @@ public class AccountCreationController {
                 ministryComboBox.setValue(null);
             }
         });
-        
+
         ministryContainer.setVisible(false);
         ministryContainer.setManaged(false);
     }
     /**
      * Handles the event when the user clicks to show the password.
      * Switches the visibility of the password fields and updates the eye icon.
+     * @param event the event that will be handled.
      */
     @FXML
     private void handleShowPassword(MouseEvent event) {
-        visiblePasswordField.requestFocus(); 
+        visiblePasswordTextField.requestFocus();
         passwordField.setVisible(false);
         passwordField.setManaged(false);
-        visiblePasswordField.setVisible(true);
-        visiblePasswordField.setManaged(true);
-        
-        Image closedEye = new Image(getClass().getResourceAsStream(EYE_CLOSED_PATH));
-        
+        visiblePasswordTextField.setVisible(true);
+        visiblePasswordTextField.setManaged(true);
+
+        Image closedEye =
+                    new Image(getClass().getResourceAsStream(EYE_CLOSED_PATH));
+
         eyeIconImageView.setImage(closedEye);
     }
     /**
      * Handles the event when the user clicks to show the confirm password.
-     * Switches the visibility of the confirm password fields and updates the eye icon.
+     * Switches the visibility of the confirm password fields
+     *                                              and updates the eye icon.
+     * @param event the event that will be handled.
      */
     @FXML
     private void handleShowConfirmPassword(MouseEvent event) {
-        confirmVisiblePasswordField.requestFocus(); 
+        confirmVisiblePasswordTextField.requestFocus();
         confirmPasswordField.setVisible(false);
         confirmPasswordField.setManaged(false);
-        confirmVisiblePasswordField.setVisible(true);
-        confirmVisiblePasswordField.setManaged(true);
-        
-        Image closedEye = new Image(getClass().getResourceAsStream(EYE_CLOSED_PATH));
+        confirmVisiblePasswordTextField.setVisible(true);
+        confirmVisiblePasswordTextField.setManaged(true);
+
+        Image closedEye =
+                    new Image(getClass().getResourceAsStream(EYE_CLOSED_PATH));
 
         confirmEyeIconImageView.setImage(closedEye);
     }
     /**
      * Handles the event when the user clicks to hide the password.
      * Switches the visibility of the password fields and updates the eye icon.
+     * @param event the event that will be handled.
      */
     @FXML
     private void handleHidePassword(MouseEvent event) {
-        passwordField.requestFocus(); 
-        visiblePasswordField.setVisible(false);
-        visiblePasswordField.setManaged(false);
+        passwordField.requestFocus();
+        visiblePasswordTextField.setVisible(false);
+        visiblePasswordTextField.setManaged(false);
         passwordField.setVisible(true);
         passwordField.setManaged(true);
 
-        Image openEye = new Image(getClass().getResourceAsStream(EYE_OPEN_PATH));
-        
+        Image openEye =
+                    new Image(getClass().getResourceAsStream(EYE_OPEN_PATH));
+
         eyeIconImageView.setImage(openEye);
     }
     /**
      * Handles the event when the user clicks to hide the confirm password.
-     * Switches the visibility of the confirm password fields and updates the eye icon.
+     * Switches the visibility of the confirm password fields and
+     *                                                  updates the eye icon.
+     * @param event the event that will be handled.
      */
     @FXML
     private void handleHideConfirmPassword(MouseEvent event) {
         confirmPasswordField.requestFocus();
-        confirmVisiblePasswordField.setVisible(false);
-        confirmVisiblePasswordField.setManaged(false);
+        confirmVisiblePasswordTextField.setVisible(false);
+        confirmVisiblePasswordTextField.setManaged(false);
         confirmPasswordField.setVisible(true);
         confirmPasswordField.setManaged(true);
 
-        Image openEye = new Image(getClass().getResourceAsStream(EYE_OPEN_PATH));
-        
+        Image openEye =
+                    new Image(getClass().getResourceAsStream(EYE_OPEN_PATH));
+
         confirmEyeIconImageView.setImage(openEye);
     }
     /**
-     * Handles the account creation process when the user clicks the create account button.
-     * Validates input and interacts with the authentication service to create a new user account.
+     * Handles the account creation process when the user clicks
+     *                                              the create account button.
+     * Validates input and interacts with the authentication service
+     *                                          to create a new user account.
      */
     @FXML
     private void handleCreateAccount() {
-        errorLabel.setVisible(false);
-        errorLabel.setText(""); 
-        successLabel.setVisible(false);
+        clearLabels();
 
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        String fullName = fullNameField.getText();
-        Ministry ministry = ministryComboBox.getValue();
-        UserRole role = roleComboBox.getValue();
+        try {
+            String username = usernameTextField.getText();
+            String password = passwordField.getText();
+            String fullName = fullNameTextField.getText();
+            Ministry ministry = ministryComboBox.getValue();
+            UserRole role = roleComboBox.getValue();
 
-        User userToValidate = null;
+            validatePassword(password);
+            String hashedPassword = PasswordUtils.hashPassword(password);
+            User userToValidate = createUser(username, fullName, hashedPassword,
+                                                        role, ministry);
 
-        if(role == UserRole.CITIZEN ) {
-            userToValidate = new Citizen(username, fullName, password);
-        } else if (role == UserRole.GOVERNMENT_MEMBER) {
-            userToValidate = new GovernmentMember(username, fullName, password, ministry);
-        } else {
-            userToValidate = PrimeMinister.getInstance(username, fullName, password);
+            if (userToValidate != null) {
+                createAccount(userToValidate, username, password,
+                                fullName, role, ministry);
+                showSuccessMessage();
+            }
+        } catch (ValidationException e) {
+                LOGGER.log(
+                    Level.INFO,
+                    "Απέτυχε το validation: {0}",
+                    e.getMessage()
+                );
+                showErrorMessage(e.getMessage());
+            }
         }
 
-        if (userToValidate != null) {
-            try {
-                InputValidationService validationService = new InputValidationService();
-                validationService.validateNewUser(userToValidate); 
-                boolean isCreated = authService.signUp(username, password, fullName, role, ministry);
-
-                if (isCreated) {
-                    successLabel.setText(Message.CREATE_ACCOUNT_SUCCESS);
-                    successLabel.setVisible(true);
-                    errorLabel.setVisible(false); 
-                } else {
-                    errorLabel.setText(Message.ERROR_INVALID_INPUT); 
-                    errorLabel.setVisible(true);
-                    successLabel.setVisible(false);
-                }
-
-            } catch (ValidationException e) {
-                errorLabel.setText(e.getMessage());
-                errorLabel.setVisible(true);
-                successLabel.setVisible(false);
-            }
+    /**
+     * Clears error and success labels.
+     */
+    private void clearLabels() {
+        errorLabel.setVisible(false);
+        errorLabel.setText("");
+        successLabel.setVisible(false);
+    }
+    /**
+     * Validates that the password meets strength requirements.
+     * @param password the password to validate
+     * @throws ValidationException if password is not strong
+     */
+    private void validatePassword(String password) throws ValidationException {
+        boolean strong = InputValidator.isPasswordStrong(password);
+        if (!strong) {
+            throw new ValidationException(
+                Message.PASSWORD_COMPLEXITY_FAIL_MESSAGE);
         }
     }
 
+    /**
+     * Creates a User object based on the provided role.
+     * @param username the username
+     * @param fullName the full name
+     * @param hashedPassword the hashed password
+     * @param role the user role
+     * @param ministry the ministry (required for GOVERNMENT_MEMBER)
+     * @return the created User object
+     */
+    private User createUser(String username, String fullName,
+                String hashedPassword, UserRole role, Ministry ministry) {
+        return switch (role) {
+            case CITIZEN -> new Citizen(username, fullName, hashedPassword);
+            case GOVERNMENT_MEMBER -> new GovernmentMember(username, fullName,
+                                                    hashedPassword, ministry);
+            case PRIME_MINISTER -> PrimeMinister.getInstance(username,
+                                                    fullName, hashedPassword);
+            };
+        }
+
+    /**
+     * Validates and creates the user account.
+     * @param userToValidate the user object to validate
+     * @param username the username
+     * @param password the plain password
+     * @param fullName the full name
+     * @param role the user role
+     * @param ministry the ministry
+     * @throws ValidationException if validation fails
+     */
+    private void createAccount(User userToValidate, String username,
+                                String password, String fullName,
+                                UserRole role, Ministry ministry)
+                                throws ValidationException {
+        InputValidationService validationService =
+                                            new InputValidationService();
+        validationService.validateNewUser(userToValidate);
+        authService.signUp(username, password, fullName, role, ministry);
+    }
+    /**
+     * Displays the success message.
+     */
+    private void showSuccessMessage() {
+        successLabel.setText(Message.CREATE_ACCOUNT_SUCCESS);
+        successLabel.setVisible(true);
+        errorLabel.setVisible(false);
+    }
+
+    /**
+     * Displays an error message.
+     * @param message the error message to display
+     */
+    private void showErrorMessage(String message) {
+        errorLabel.setText(message);
+        errorLabel.setVisible(true);
+        successLabel.setVisible(false);
+    }
+    /**
+     * Resets all the field.
+     */
     @FXML
     private void handleCancel() {
-        usernameField.clear();
+        usernameTextField.clear();
         passwordField.clear();
         confirmPasswordField.clear();
-        fullNameField.clear();
+        fullNameTextField.clear();
         roleComboBox.setValue(null);
         ministryComboBox.setValue(null);
         errorLabel.setVisible(false);
         successLabel.setVisible(false);
     }
-
+    /**
+     * Handles navigation to the Welcome View when user presses
+     *                                                  the back button.
+     */
     @FXML
     private void handleBack() {
         try {
             Stage stage = (Stage) backButton.getScene().getWindow();
-            SceneLoader loader = new SceneLoader(stage);
-            loader.load("/view/WelcomeView.fxml", "Welcome");
+            SceneLoader.load(stage, "/view/WelcomeView.fxml", "Welcome");
         } catch (Exception e) {
             e.printStackTrace();
         }
